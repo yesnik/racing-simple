7885ac8978b33d77f21a14b80a3c7d5b9487ad2c
Nikita Nyatin <yesnik@yandex.ru>
2017-12-23 10:36:15 +0500

    Initial commit

---
+++ app/dist/main.js
@@ -0,0 +1,9 @@
+"use strict";
+var Cat = (function () {
+    function Cat() {
+        this.name = 'Kesha';
+        this.age = 2;
+    }
+    return Cat;
+}());
+//# sourceMappingURL=main.js.map
---
+++ app/src/main.ts
@@ -0,0 +1,4 @@
+class Cat {
+  name = 'Kesha'
+  age = 2
+}
