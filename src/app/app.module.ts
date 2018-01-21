187d80075a76431f658e77462c0fdaa1f9f718bf
Nikita Nyatin <yesnik@yandex.ru>
2018-01-21 14:52:20 +0500

    Add HttpClient to app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { CarPartsComponent } from './car-parts.component'
@@ -13,6 +13,7 @@ import { CarPartsComponent } from './car-parts.component'
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     FormsModule
   ],
   providers: [],
--- src/app/racing-data.service.ts
+++ src/app/racing-data.service.ts
@@ -1,9 +1,12 @@
 import { Injectable } from '@angular/core';
 import { CARPARTS } from './mocks';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable()
 export class RacingDataService {
   getCarParts() {
     return CARPARTS;
   }
+
+  constructor(private http: HttpClient) { }
 }
